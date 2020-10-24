d749cbb6cefef54004636847edaeb5bdc2218305
Zulfan Hameed <zlfnhmd@gmail.com>
2020-10-24 16:40:19 +0530

    create template

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a starter template for [Next.js](https://nextjs.org).
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+import Layout from '../components/layout';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title></title>
+      </Head>
+      <section></section>
+    </Layout>
+  );
+}
